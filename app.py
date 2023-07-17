d4becd3ca81b3904113063b3ca34afadf111a1f0
Danielifed <d.danielife@gmail.com>
2023-07-17 17:20:11 +0100

    update

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 app.secret_key = 'Ifedaniel@0704'
 
 #configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ifedaniel@0704'
+app.config['MYSQL_HOST'] = 'aws.connect.psdb.cloud'
+app.config['MYSQL_USER'] = 'e32m5rf7dwc2ekmhtp9x'
+app.config['MYSQL_PASSWORD'] = 'pscale_pw_XXMMmJKrIbjSYlcNOeDXXweaGXLTy4zXVzUtTFtgz9S'
 app.config['MYSQL_DB'] = 'didnow'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
