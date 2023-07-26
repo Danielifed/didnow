65c7e8a86e2ec24ceea8adb62eb178cf6bd90834
Danielifed <d.danielife@gmail.com>
2023-07-26 11:17:47 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}
 #get file from .env
 connection = MySQLdb.connect(
-  host= os.getenv("HOST"),
+  host=os.getenv("HOST"),
   user=os.getenv("USERNAME"),
-  passwd= os.getenv("PASSWORD"),
-  db= os.getenv("DATABASE"),
-  autocommit = True,
-  ssl_mode = "VERIFY_IDENTITY"
+  passwd=os.getenv("PASSWORD"),
+  db=os.getenv("DATABASE"),
+  autocommit=True,
+  ssl_mode="VERIFY_IDENTITY"
 )
 
 #init MySQL
