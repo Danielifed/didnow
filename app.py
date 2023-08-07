a486317224843b4ff2c516aa9b59bb07f890f2df
Danielifed <d.danielife@gmail.com>
2023-08-07 08:34:21 +0100

    update

--- app.py
+++ app.py
@@ -18,17 +18,17 @@ app = Flask(__name__)
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}
 #get file from .env
-connection = MySQLdb.connect(
-  host= os.getenv("HOST"),
-  user=os.getenv("USERNAME"),
-  passwd= os.getenv("PASSWORD"),
-  db= os.getenv("DATABASE"),
-  autocommit = True,
-  ssl_mode = "VERIFY_IDENTITY",
-  ssl      = {
-    "ca": "/etc/ssl/certs/ca-certificates.crt"
-  }
-)
+#connection = MySQLdb.connect(
+  #host= os.getenv("HOST"),
+  #user=os.getenv("USERNAME"),
+  #passwd= os.getenv("PASSWORD"),
+  #db= os.getenv("DATABASE"),
+  #autocommit = True,
+  #ssl_mode = "VERIFY_IDENTITY",
+  #ssl      = {
+   # "ca": "/etc/ssl/certs/ca-certificates.crt"
+  #}
+#)
 #init MySQL
 mysql = MySQL(app)
 
